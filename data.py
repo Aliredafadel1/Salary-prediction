d16aea36500e6c421fa908f353063b76a05c48f4
Aliredafadel1 <fadelali0123@gmail.com>
2026-04-07 01:52:50 +0300

    first commit

--- data.py
+++ data.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-url = "https://raw.githubusercontent.com/Aliredafadel1/salary-prediction/main/ds_salaries.csv"
-df = pd.read_csv(url)
+import pandas as pd 
+df=pd.read_csv('ds_salaries.csv')
 print(df.head())
-pd.set_option('display.max_columns', 50)
